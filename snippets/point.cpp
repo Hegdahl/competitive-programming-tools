db32a2ea543edd8ba79f30e32b8038e88975deeb
Hegdahl <magnus.hegdahl@gmail.com>
2021-06-26 18:28:28 +0200

    make coord type templated and make stuff constexpr

--- snippets/point.cpp
+++ snippets/point.cpp
@@ -5,24 +5,25 @@ using ll = long long;
 using ld = long double;
 
 /*BEGIN_SNIPPET*/
+template<class T = ll>
 struct Point {
-  ll x, y;
-  Point() : x(0), y(0) {}
-  Point(ll x, ll y) : x(x), y(y) {}
+  T x, y;
+  constexpr Point() : x(0), y(0) {}
+  constexpr Point(T x_, T y_) : x(x_), y(y_) {}
 
-  Point &operator+=(const Point &o) { x += o.x, y += o.y; return *this; }
-  Point &operator-=(const Point &o) { x -= o.x, y -= o.y; return *this; }
-  Point operator+(const Point &o) const { Point r = *this; r += o; return r; }
-  Point operator-(const Point &o) const { Point r = *this; r -= o; return r; }
+  constexpr Point &operator+=(const Point &o) { x += o.x, y += o.y; return *this; }
+  constexpr Point &operator-=(const Point &o) { x -= o.x, y -= o.y; return *this; }
+  constexpr Point operator+(const Point &o) const { Point r = *this; r += o; return r; }
+  constexpr Point operator-(const Point &o) const { Point r = *this; r -= o; return r; }
 
-  friend bool operator==(const Point &a, const Point &b) { return a.x==b.x && a.y==b.y; }
-  friend bool operator!=(const Point &a, const Point &b) { return a.x!=b.x || a.y!=b.y; }
+  constexpr friend bool operator==(const Point &a, const Point &b) { return a.x==b.x && a.y==b.y; }
+  constexpr friend bool operator!=(const Point &a, const Point &b) { return a.x!=b.x || a.y!=b.y; }
 
-  friend ll cross(const Point &a, const Point &b) { return a.x*b.y - b.x*a.y; }
-  friend ll dot(const Point &a, const Point &b) { return a.x*b.x + b.y*a.y; }
-  ll curl(const Point &a, const Point &b) const { return cross(a-*this, b-*this); }
+  constexpr friend T cross(const Point &a, const Point &b) { return a.x*b.y - b.x*a.y; }
+  constexpr friend T dot(const Point &a, const Point &b) { return a.x*b.x + b.y*a.y; }
+  constexpr T curl(const Point &a, const Point &b) const { return cross(a-*this, b-*this); }
 
-  ll mag_sq() const { return x*x + y*y; }
+  T mag_sq() const { return x*x + y*y; }
   ld mag() const { return sqrt((ld)mag_sq()); }
 
   friend ostream &operator<<(ostream &os, const Point &p) { return os << p.x << ' ' << p.y; }
@@ -40,5 +41,6 @@ struct Point {
 /*END_SNIPPET*/
 
 int main() {
-  // TODO: make tests
+  static_assert(Point().curl(Point(1LL, 0LL), Point(0LL, 1LL)) == 1);
+  static_assert(Point().curl(Point(0LL, 1LL), Point(1LL, 0LL)) == -1);
 }
