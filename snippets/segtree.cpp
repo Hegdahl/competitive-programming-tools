ba382ecccf955bacd1e34cedac417be497b69c3c
Hegdahl <magnus.hegdahl@gmail.com>
2021-06-26 21:33:34 +0200

    replace iterative lazy with recursive lazy beats

--- snippets/segtree.cpp
+++ snippets/segtree.cpp
@@ -1,177 +1,212 @@
-#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
+#define ar array
 
 using namespace std;
 using ll = long long;
 
 /*BEGIN_SNIPPET*/
-template<class S, class F>
-struct SegTreeBase {
-  
+template<class S, class F = std::nullptr_t>
+struct SegTree {
+  static constexpr bool is_lazy = !std::is_same<F, std::nullptr_t>::value;
+
+  int n, offset;
   vector<S> values;
-  vector<F> queued;
-  int lg2, offset;
+  vector<F> lazy;
 
-  SegTreeBase(const vector<S> &src)
-    : lg2(__lg((int)src.size()-1)+1), offset(2<<__lg((int)src.size()-1)) {
-    values.reserve(2*offset);
-    values.resize(offset);
-    values.insert(values.end(), src.begin(), src.end());
+  SegTree(int n_) : n(n_), offset(2<<__lg(n-1)) {
     values.resize(2*offset);
-    queued.resize(offset);
-    for (int i = offset-1; i > 0; --i) recalc(i);
+    if constexpr (is_lazy) lazy.resize(offset);
+  }
+  SegTree(int n_, S) : SegTree(n_) {}
+  SegTree(int n_, S, F) : SegTree(n_) {}
+
+  struct RangeReference {
+    SegTree &st;
+    const int i, j;
+
+    RangeReference(SegTree &st_, int i_, int j_) : st(st_), i(i_), j(j_) {}
+
+    template<typename Container>
+    auto &operator=(const Container &s) {
+      if constexpr (is_lazy) st.push_all(i, j, 1, 0, st.offset-1);
+
+      int k = i;
+      auto it = begin(s);
+      while (it != end(s))
+        st.values[st.offset + k++] = *it++;
+      assert(k == j+1);
+
+      int I = i+st.offset, J = j+st.offset;
+      while ((I /= 2) && (J /= 2))
+        for (k = I; k <= J; ++k)
+          st.values[k].pull(st.values[2*k], st.values[2*k+1]);
+
+      return *this;
+    }
+
+    auto &operator=(S s) {
+      if constexpr (is_lazy) st.push_all(i, j, 1, 0, st.offset-1);
+      for (int k = i; k <= j; ++k)
+        st.values[st.offset+k] = s;
+
+      int I = i+st.offset, J = j+st.offset;
+      while ((I /= 2) && (J /= 2))
+        for (int k = I; k <= J; ++k)
+          st.values[k].pull(st.values[2*k], st.values[2*k+1]);
+
+      return *this;
+    }
+
+    auto &operator*=(F f) {
+      static_assert(is_lazy);
+      st.upd(i, j, f, 1, 0, st.offset);
+      return *this;
+    }
+
+    operator S() {
+      return st.qry(i, j, 1, 0, st.offset-1);
+    }
+  };
+
+  auto operator[](int i) {
+    return RangeReference(*this, i, i);
+  }
+  auto operator[](pair<int,int> ij) {
+    return RangeReference(*this, ij.first, ij.second);
   }
 
-  void recalc(int I) {
-    assert(I > 0 && I < offset);
-    values[I] = values[2*I] * values[2*I+1];
-  }
+  int push(int I, int l, int r) {
+    static_assert(is_lazy);
+    assert(I > 0);
+    assert(I < offset);
 
-  void push(int I) {
-    assert(I > 0 && I < offset);
-    values[2*I] = queued[I] * values[2*I];
-    values[2*I+1] = queued[I] * values[2*I+1];
+    lazy[I].apply(values[2*I]);
+    lazy[I].apply(values[2*I+1]);
     if (2*I < offset) {
-      queued[2*I] = queued[I] * queued[2*I];
-      queued[2*I+1] = queued[I] * queued[2*I+1];
+      lazy[I].apply(lazy[2*I]);
+      lazy[I].apply(lazy[2*I+1]);
     }
-    queued[I] = F();
+    lazy[I] = F();
+
+    return l + (r-l+1)/2;
   }
 
-  void push_col(int I) {
-    assert(I >= offset && I < 2*offset);
-    for (int lvl = lg2-1; lvl >= 0; --lvl)
-      push((I>>lvl)/2);
+  void push_all(int i, int j, int I, int l, int r) {
+    static_assert(is_lazy);
+    if (l == r) return;
+
+    int mid = push(I, l, r);
+    if (i < mid) push_all(i, j, 2*I, l, mid-1);
+    if (j >= mid) push_all(i, j, 2*I+1, mid, r);
   }
 
-  void push_range(int I, int J) {
-    assert(I+1 >= offset && I+1 < 2*offset);
-    assert(J-1 >= offset && J-1 < 2*offset);
-    assert(I+1 < J);
-    for (int lvl = lg2-1; lvl >= 0; --lvl) {
-      push(((I+1)>>lvl)/2);
-      if (I+1 != J-1) push(((J-1)>>lvl)/2);
+  S qry(int i, int j, int I, int l, int r) {
+    assert(!(i > r || j < l));
+
+    if (i <= l && j >= r) return values[I];
+
+    int mid;
+    if constexpr (is_lazy) mid = push(I, l, r);
+    else mid = l + (r-l+1)/2;
+
+    S res; res.pull(
+        (i < mid ? qry(i, j, 2*I, l, mid-1) : S()),
+        (j >= mid ? qry(i, j, 2*I+1, mid, r) : S())
+    );
+    return res;
+  }
+
+  void upd(int i, int j, F f, int I, int l, int r) {
+    static_assert(is_lazy);
+    assert(!(i > r || j < l));
+    if (i <= l && j >= r && f.can_lazy(values[I])) {
+      f.apply(values[I]);
+      if (I < offset)
+        f.apply(lazy[I]);
+      return;
     }
-  }
 
-  void set(int i, S v) {
-    assert(i >= 0 && i < offset);
-    i += offset;
-    push_col(i);
-    values[i] = v;
-    while (i /= 2) recalc(i);
+    assert(l != r);
+    int mid = push(I, l, r);
+    
+    if (i < mid) upd(i, j, f, 2*I, l, mid-1);
+    if (j >= mid) upd(i, j, f, 2*I+1, mid, r);
+    values[I].pull(values[2*I], values[2*I+1]);
   }
-
-  void upd_node(int I, const F &f) {
-    assert(I > 0 && I < 2*offset);
-    values[I] = f * values[I];
-    if (I < offset) queued[I] = f * queued[I];
-  }
-
-  void upd(int i, const F &f) {
-    assert(i >= 0 && i < offset);
-    i += offset;
-    push_col(i);
-    upd_node(i, f);
-    while (i/=2) recalc(i);
-  }
-
-  void upd(int i, int j, const F &f) {
-    assert(i >= 0 && i < offset);
-    assert(j >= 0 && j < offset);
-    assert(i <= j);
-    i += offset-1;
-    j += offset+1;
-    int oi = i, oj = j;
-    push_range(i, j);
-    while (i+1 < j) {
-      if ((i&1)==0) upd_node(i+1, f);
-      if ((j&1)==1) upd_node(j-1, f);
-      i >>= 1, j >>= 1;
-    }
-    i = oi+1, j = oj-1;
-    int min_lvl_i = __builtin_ctz(i);
-    int min_lvl_j = __builtin_ctz(j+1);
-    for (int lvl = 1; lvl <= lg2; lvl++) {
-      if (min_lvl_i < lvl) recalc(i >> lvl);
-      if (min_lvl_j < lvl) recalc(j >> lvl);
-    }
-  }
-
-  S qry(int i) {
-    assert(i >= 0 && i < offset);
-    i += offset;
-    push_col(i);
-    return values[i];
-  }
-
-  S qry(int i, int j) {
-    assert(i >= 0 && i < offset);
-    assert(j >= 0 && j < offset);
-    assert(i <= j);
-    i += offset-1;
-    j += offset+1;
-    push_range(i, j);
-    S ls, rs;
-    while (i+1 < j) {
-      if ((i&1)==0) ls = ls * values[i+1];
-      if ((j&1)==1) rs = values[j-1] * rs;
-      i >>= 1, j >>= 1;
-    }
-    return ls * rs;
-  }
-
 };
 
 struct S {
-  ll w = 1;
-  ll s = 0;
-  S operator*(const S &rhs) const {
-    return {w+rhs.w, s+rhs.s};
+  ll w = 0, sum = 0; 
+  ar<ll, 2> mx = {-1LL<<60, 0}, mx2 = {-1LL<<60, 0};
+
+  S() {}
+  S(ll v) : w(1), sum(v), mx({v, 1}) {}
+
+  void pull(S l, S r) {
+    w = l.w + r.w;
+    sum = l.sum + r.sum;
+
+    ar v {l.mx, l.mx2, r.mx, r.mx2};
+    sort(v.rbegin(), v.rend());
+    for (int i = 2; i >= 0; --i)
+      if (v[i][0] == v[i+1][0]) v[i][1] += v[i+1][1], v[i+1] = {-1LL<<60, 0};
+    sort(v.rbegin(), v.rend());
+
+    mx = v[0], mx2 = v[1];
   }
 };
 
 struct F {
-  bool change_to = false;
-  ll sum = 0;
-  F operator*(const F &rhs) const {
-    if (change_to) return *this;
-    return {rhs.change_to, rhs.sum+sum};
+  ll add = 0, mn = 1LL<<60;
+
+  bool can_lazy(S s) {
+    return mn > s.mx2[0] + add;
   }
-  S operator*(const S &rhs) const {
-    if (change_to) return {rhs.w, sum * rhs.w};
-    return {rhs.w, rhs.s + sum * rhs.w};
+
+  void apply(F &f) {
+    f.add += add;
+    f.mn += add;
+    f.mn = min(mn, f.mn);
+  }
+
+  void apply(S &s) {
+    assert(s.mx2[0]+add < mn);
+
+    s.sum += add * s.w;
+    s.mx[0] += add;
+    s.mx2[0] += add;
+
+    if (mn > s.mx[0]) return;
+
+    ll d = s.mx[0] - mn;
+    s.sum -= d * s.mx[1];
+    s.mx[0] = mn;
   }
 };
-
-using SegTree = SegTreeBase<S, F>;
 /*END_SNIPPET*/
 
 int main() {
-  // solution for https://cses.fi/problemset/task/1735/ is used as a test
-  ios::sync_with_stdio(0);cin.tie(0);
+  cin.tie(0)->sync_with_stdio(0);
 
-  int n, q; cin >> n >> q;
-  vector<S> a(n);
-  for (int i = 0; i < n; ++i)
-    cin >> a[i].s;
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int &x : a) cin >> x;
 
-  SegTree st(a);
+  SegTree st(n, S{}, F{});
+  st[{0, n-1}] = a;
 
+  int q; cin >> q;
   for (int qq = 0; qq < q; ++qq) {
-    char c; cin >> c;
-    if (c == '1') {
-      int i, j; ll u;
-      cin >> i >> j >> u; --i, --j;
-      st.upd(i, j, {false, u});
-    } else if (c == '2') {
-      int i, j; ll u;
-      cin >> i >> j >> u; --i, --j;
-      st.upd(i, j, {true, u});
-    } else if (c == '3') {
+    int t; cin >> t;
+    if (t == 1) {
+      int i, j, x; cin >> i >> j >> x; --i, --j;
+      st[{i, j}] *= {0, x};
+    } else if (t == 2) {
+      int i, j, x; cin >> i >> j >> x; --i, --j;
+      st[{i, j}] *= {x, 1LL<<60};
+    } else if (t == 3) {
       int i, j; cin >> i >> j; --i, --j;
-      cout << st.qry(i, j).s << '\n';
-    } else assert(0);
+      cout << S(st[{i, j}]).sum << '\n';
+    }
   }
-
 }
