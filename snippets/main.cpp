ff9abd954b3d7e6960be208451d6a727629c1e60
Hegdahl <magnus.hegdahl@gmail.com>
2022-02-10 19:48:27 +0100

    more default includes

--- snippets/main.cpp
+++ snippets/main.cpp
@@ -1,6 +1,15 @@
 /*BEGIN_SNIPPET*/
 #include <algorithm>
+#include <cassert>
+#include <functional>
 #include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <utility>
 #include <vector>
 
 int main() {
